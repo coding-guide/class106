0a7a7799ee12887310c0eeca6e31a972f0e5ebb6
sunrye <srkim8744@korea.kr>
2020-08-28 10:34:46 +0900

    switch

---
+++ src/controller/IfExam03.java
@@ -0,0 +1,20 @@
+package controller;
+
+import java.util.Scanner;
+
+public class IfExam03 {
+
+	public static void main(String[] args) {
+//	해결과제 if~elseif문 이용하기
+// 사용자로부터 순위를 입력받습니다
+// 1, 2, 3, ....
+// 1이면 "금메일", 2이면 "은메달", 3이면 "동메달", 
+//	그외는 "수상하지 못했습니다"
+//	를 출력하세요
+		Scanner in = new Scanner(System.in);
+		int rank = in.nextInt();
+//		여기부터 if~elseif문으로 문제해결하기
+//		A와 B가 같으면? if(A==B)
+	}
+
+}
